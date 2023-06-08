6b53d0d27283481ff0bf1f52db37181a63b00201
SHARTS7 <117847968+SHARTS7@users.noreply.github.com>
2023-06-08 15:14:45 -0300

    Jun +Alterações
    Alterações iniciais do mês de Junho para previa TCC (apresentação)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function(){
+setInterval(function () {
 	nextImage();
 }, 5000)
 
-function nextImage(){
+function nextImage() {
 	count++;
-	if(count > 4){
+	if (count > 4) {
 		count = 1
 	}
 
-	document.getElementById("radio"+count).checked = true;
+	document.getElementById("radio" + count).checked = true;
+}
+
+function openComents(){
+	const modal_coments = document.getElementById('coments-modal')
+	modal_coments.classList.add('abrir')
+
+	modal_coments.addEventListener('click', (e) => {
+		if(e.target.id == 'close' || e.target.id == 'coments-modal'){
+			modal_coments.classList.remove('abrir')
+		}
+	})
 
 }
